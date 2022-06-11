c85dfeb8ef1b73eb07929e2ff424265e06788774
Michallesss <mwiemszra@gmail.com>
2022-06-11 13:10:01 +0200

    comments and code optimalization update

--- app/quicksort.cpp
+++ app/quicksort.cpp
@@ -4,21 +4,29 @@
 using namespace std;
 
 void quicksort(int tab[], int left, int right) {
-    int v=tab[(left+right)/2];
-    int i,j,x;
-    i=left; 
-    j=right;
+    int v=tab[(left+right)/2]; //checking pivot(in this situation it's middle point)
+    int i,j,x;  //defining variables
+    i=left; //i==start point(left)
+    j=right; //i==end point(right)
     do {
-        while(tab[i]<v) i++;
-        while(tab[j]>v) j--;
+        while(tab[i]<v) {
+            i++; 
+        } 
+        while(tab[j]>v) {
+            j--;
+        } 
         if(i<=j) {
-            x=tab[i];
+            x=tab[i];   //swapping..
             tab[i]=tab[j];
             tab[j]=x;
             i++;
             j--;
         }
     } while(i<=j);
-    if(j>left) quicksort(tab, left, j);
-    if(i<right) quicksort(tab, i, right);
+    if(j>left) {
+        quicksort(tab, left, j);
+    } 
+    if(i<right) {
+        quicksort(tab, i, right);
+    }
 }
