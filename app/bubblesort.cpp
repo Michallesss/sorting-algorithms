f6ead560a8e407831456538229788ec4fb5565b4
Michallesss <mwiemszra@gmail.com>
2022-06-11 13:09:39 +0200

    comments and optimalization update

--- app/bubblesort.cpp
+++ app/bubblesort.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 void bubblesort(int tab[], int size) {
+    int size2=size;
     for(int j=0; j<size-1; j++) {
-        for(int i=0; i<size-1; i++) {
-            if(tab[i]>tab[i+1]) {
-                int x=tab[i];
+        for(int i=0; i<size2-1; i++) {
+            if(tab[i]>tab[i+1]) { //if left point > right point
+                int x=tab[i];     //swaping..  
                 tab[i]=tab[i+1];
                 tab[i+1]=x;
             }
